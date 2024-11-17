37261cada8a2ab09742e26089e3095e746fdfc36
neidymlg <neidymlg@gmail.com>
2024-11-16 21:55:59 -0800

    Changes to order

--- SleepingTa.c
+++ SleepingTa.c
@@ -8,11 +8,11 @@
 pthread_t *Students; //N threads running as Students.
 pthread_t TA; //Separate Thread for TA.
 
-
 sem_t TA_sleep; //A semaphore to signal and wait TA's sleep.
 sem_t student_leave; //A semaphore to signal and wait for student to fully leave
 sem_t TA_available; //A semaphore to allow only one person to go through TA
 sem_t chairs_sem; //A semaphore to allow one person to sit in chair after someone leaves
+
 pthread_mutex_t mutex; 
 
 void *TA_Activity();
@@ -25,13 +25,31 @@ int main(int argc, char* argv[])
 				//student threads. Default is 5 student threads.
 
 	//Initializing Mutex Lock and Semaphores.
-	pthread_mutex_init(&mutex, NULL);
-	
-	sem_init(&TA_sleep, 0, 0);
-	sem_init(&TA_available, 0, 0);
-	sem_init(&student_leave, 0, 0);
-	sem_init(&chairs_sem, 0, WR);
+ 	if (pthread_mutex_init(&mutex, NULL) != 0) {
+        	perror("pthread_mutex_init");
+       	 	exit(EXIT_FAILURE);
+   	 }
 
+    	if (sem_init(&TA_sleep, 0, 0) != 0) {
+       		perror("sem_init TA_sleep");
+       	 	exit(EXIT_FAILURE);
+   	 }
+   	 
+    	if (sem_init(&TA_available, 0, 0) != 0) {
+       		perror("sem_init TA_available");
+       		exit(EXIT_FAILURE);
+    	}
+    	
+   	if (sem_init(&student_leave, 0, 0) != 0) {
+        	perror("sem_init student_leave");
+        	exit(EXIT_FAILURE);
+   	 }
+   	 
+  	if (sem_init(&chairs_sem, 0, WR) != 0) {
+        	perror("sem_init chairs_sem");
+        	exit(EXIT_FAILURE);
+    	}
+    	
 	if(argc<2)
 	{	
 		printf("Number of Students not specified. Using default (5) students.\n");
@@ -49,7 +67,7 @@ int main(int argc, char* argv[])
 	Students = (pthread_t*) malloc(sizeof(pthread_t)*number_of_students);
 
 	pthread_create(&TA, NULL, TA_Activity, NULL);
-
+	
 	//Creating one TA thread and N Student threads.
 	for (int i = 0; i < number_of_students; i++) {
 		id[i] = i;
@@ -81,8 +99,8 @@ void *TA_Activity()
 {
 	while(1){//if chairs are empty, break the loop.
 		printf("TA is sleeping.\n");
-		sem_wait(&TA_sleep); //TA is currently sleeping, waits for Students to wake them up
-		sem_post(&TA_available); //only allows one student to go in
+		sem_wait(&TA_sleep); //TA is currently sleeping.
+		sem_post(&TA_available);
 		int sleep_time = rand() % 6 + 5;
 		sleep(sleep_time);//Student is being helped by TA
 		sem_post(&student_leave);	
@@ -94,33 +112,29 @@ void *Student_Activity(void *threadID)
 {
 	int id = *((int *)threadID);
 
-	//For sitting in Waiting Room
-	//one student enters the room one at a time
-	//if a chair is empty, a student will take it 
 	pthread_mutex_lock(&mutex);
-	if(sem_trywait(&chairs_sem) == 0) {
-		printf("Student %d takes a chair.\n", id);
+	
+	//For sitting in Waiting Room 
+	if(sem_trywait(&chairs_sem) != 0) {
+		printf("Student %d couldn't find a seat and has left. Student will return later.\n", id);
+		pthread_mutex_unlock(&mutex);
+		sem_wait(&chairs_sem);
+		
 	}
 	else{
 		pthread_mutex_unlock(&mutex);
-		//student couldn't find a seat, will wait for a seat to beocme available 
-		printf("Student %d couldn't find a seat and has left. Student will return later.\n", id);
-		
-		sem_wait(&chairs_sem);
-		
-		pthread_mutex_lock(&mutex);
-		printf("Student %d takes a chair\n", id);	
 	}
-
+	
 	//For getting help from TA
-	printf("Student %d is waiting in chair.\n", id);
-	sem_post(&TA_sleep); //students try to wake up TA
+	pthread_mutex_lock(&mutex);
+	printf("Student %d takes a chair\n", id);	
+	sem_post(&TA_sleep);
 	pthread_mutex_unlock(&mutex);
 	
-	sem_wait(&TA_available); //students wait their turn, only one student enters to get help
+	sem_wait(&TA_available);
 	printf("Student %d wakes up TA and heads into office.\n", id);
-	sem_post(&chairs_sem); //student has left chair, now a new student can occupy iy
-	sem_wait(&student_leave); //waits for student to leave after they have gotten help
+	sem_post(&chairs_sem);
+	sem_wait(&student_leave);
 
 	pthread_exit(NULL);
 }
